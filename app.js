0316e4b83d8eeadef613f0ec3439d1954ecf90c3
jayakrishna2003 <109348724+jayakrishna2003@users.noreply.github.com>
2024-03-16 13:37:18 +0530

    one test case error

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 
 const app = express()
 
-app.get('./gadgets', (request, response) => {
-  response.sendFile('./gadgets.html', {root: __dirname})
-})
+// app.get('./gadgets', (request, response) => {
+//   response.sendFile('./gadgets.html', {root: __dirname})
+// })
+app.get("/gadgets", (request, response) => {
+  response.sendFile(path.join(__dirname, "gadgets.html"));
+});
 
 module.exports = app
